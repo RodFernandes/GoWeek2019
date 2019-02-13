bce74935feda5c5c8e17d022691799e93fc3a84d
RodFernandes <rodfernandes82@gmail.com>
2019-02-13 23:22:17 +0000

    routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 //mLab.com
-mongoose.connect('mongodb://goweek:goweek123@ds223605.mlab.com:23605/goweek-backend', {
+mongoose.connect(
+  "mongodb://goweek:goweek123@ds223605.mlab.com:23605/goweek-backend",
+  {
     useNewUrlParser: true
-});
+  }
+);
 
-app.get('/', (req, res) => {
-    return res.send('Hello World Lica');
-});
+app.use(require("./routes"));
 
 app.listen(3000, () => {
-    console.log("Server Started on Port 3000");
+  console.log("Server Started on Port 3000");
 });
