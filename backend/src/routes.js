5a4c303a3f67a3940cb8e63e857dbe46394998ee
RodFernandes <rodfernandes82@gmail.com>
2019-02-15 23:30:21 +0000

    Tweet Likes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,10 @@ const express = require("express");
 const routes = express.Router();
 
 const TweetController = require("./controllers/TweetController");
+const LikesControlller = require("./controllers/LikeController");
 
 routes.get("/tweets", TweetController.index);
 routes.post("/tweets", TweetController.store);
+routes.post("/likes/:id", LikesControlller.store);
 
 module.exports = routes;
