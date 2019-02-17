1f177add406003f45763b18e38d272f2391284f1
RodFernandes <rodfernandes82@gmail.com>
2019-02-17 00:52:09 +0000

    tweet

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
 
+import twitterLogo from "../twitter.svg";
+import "./Timeline.css";
+
 export default class TimeLine extends Component {
+  state = {
+    newTweete: ""
+  };
+
+  handleNewTweet = e => {
+    if (e.keyCode !== 13) return;
+
+    const content = this.state.newTweete;
+    const author = localStorage.getItem("@GoTwitter:username");
+
+    console.log(content, author);
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTweete: e.target.value });
+  };
+
   render() {
-    return <h1>TimeLine</h1>;
+    return (
+      <div className="timeline-wrapper">
+        <img height={24} src={twitterLogo} alt="GoTwitter" />
+        <form>
+          <textarea
+            value={this.state.newTweete}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}
+            placeholder="O que esta acontecendo?"
+          />
+        </form>
+      </div>
+    );
   }
 }
